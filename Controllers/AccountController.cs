56aea88b78a4d5eebba6910225a0ce50095f3db4
nosana <novanopan26@gmail.com>
2022-11-03 21:06:10 +0700

    commit first

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,95 @@
+﻿using API.Repository.Data;
+using API.ViewModels;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private AccountRepository _accountRepository;
+        public AccountController(AccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+        [HttpPost]
+        public ActionResult Register(string FullName, string Email, DateTime BirthDate, string Password)
+        {
+            try
+            {
+                var result = _accountRepository.Register(FullName, Email, BirthDate, Password);
+                if (result == 0)
+                {
+                    return Ok(new
+                    {
+                        StatusCode = 200,
+                        Message = "Register Tidak Berhasil"
+
+                    });
+                }
+                else
+                {
+                    return Ok(new
+                    {
+                        StatusCode = 200,
+                        Message = "Register Berhasil"
+                    });
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    StatusCode = 400,
+                    Message = ex.Message
+                });
+            }
+        }
+
+
+        /*public ActionResult ChangePassword(string Passnew, string Password)
+        {
+
+        }*/
+        /* public ActionResult Login(LoginVM login)
+        {
+            try
+            {
+                var data = _accountRepository.Login(login.Email,login.Password);
+                if(data == null)
+                {
+                    return Ok(new
+                    {
+                        StatusCode = 200,
+                        Message = "Data Tidak Ditemukan"
+
+                    });
+                }
+                else
+                {
+                    return Ok(new
+                    {
+                        StatusCode = 200,
+                        Message = "Data Ditemukan",
+                        Data = data
+
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    StatusCode = 400,
+                    Message = ex.Message
+                });
+            }
+        }*/
+
+
+    }
+}
---
+++ Repository/Data/AccountRepository.cs
@@ -0,0 +1,124 @@
+﻿using API.Context;
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace API.Repository.Data
+{
+    public class AccountRepository
+    {
+        private MyContext _context;
+
+        public AccountRepository(MyContext context)
+        {
+            _context = context;
+        }
+
+       /* public int Login(LoginVM login)
+        {
+
+            var data = _context.Users
+                  .Include(x => x.Employee)
+                  .Include(x => x.Role)
+                  .SingleOrDefault(x => x.Employee.Email.Equals(login.Email));
+            if (data != null)
+            {
+                
+
+            }
+            return 0;
+
+        }*/
+
+        public int Register(string FullName, string Email, DateTime BirthDate, string Password)
+        {
+            var data = _context.Users
+                .Include(x => x.Employee)
+                .SingleOrDefault(x => x.Employee.Email.Equals(Email));
+            if (data == null)
+            {
+                Employee employee = new Employee()
+                {
+                    Email = Email,
+                    FullName = FullName,
+                    BirthDate = BirthDate
+                };
+                _context.Employees.Add(employee);
+                var result = _context.SaveChanges();
+                if (result >0)
+                {
+                    var id = _context.Employees.SingleOrDefault(x => x.Email.Equals(Email)).Id;
+                    User user = new User()
+                    {
+                        Id = id,
+                        Password = Password,
+                        RoleId = 2
+                    };
+                    _context.Users.Add(user);
+                    var resultUser = _context.SaveChanges();
+                    if (resultUser > 0)
+                    {
+                        return resultUser;
+                    }
+                    return result;
+                }
+                return 0;
+            }
+            return 0 ;
+        }
+       
+        
+
+        public int ChangePassword(string Passnew, string Password)
+        {
+            var data = _context.Users
+               .Include(x => x.Employee)
+               .SingleOrDefault(x => x.Employee.Email.Equals(Password));
+            if (data != null)
+            {
+
+                if (data.Password == Passnew)
+                {
+                    data.Password = Passnew;
+                    _context.Entry(data).State = EntityState.Modified;
+                    var result = _context.SaveChanges();
+                    if (result > 0)
+                    {
+                        return result;
+                    }
+                    return 0;
+                }
+                return 0;
+            }
+
+            return 0;
+        }
+        public int ForgotPassword(string Fullname, string Email, string NewPass)
+        {
+
+            var data = _context.Users
+            .Include(x => x.Employee)
+
+            .SingleOrDefault(x => x.Employee.Email.Equals(Email) && x.Employee.FullName.Equals(Fullname));
+
+            if (data != null)
+            {
+
+                data.Password = NewPass;
+
+                _context.Entry(data).State = EntityState.Modified;
+
+                var result = _context.SaveChanges();
+                if (result > 0)
+                {
+                    return result;
+                }
+                return 0;
+            }
+            return 0;
+        }
+        
+        
+    }
+}
